e13077b1273ea58c1c7de4dd8d985ec1daaef699
Mblkolo <rusakov-rv@yandex.ru>
2017-07-06 00:04:19 +0500

    Добавил репозиторий групп

---
+++ WebAPI/Controllers/GroupController.cs
@@ -0,0 +1,79 @@
+﻿using System.Web.Http;
+using WebAPI.Dto;
+using WebAPI.Dal;
+using System.Threading.Tasks;
+using System;
+using System.Security.Claims;
+using System.Linq;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class GroupController : ApiController
+    {
+        private readonly IGroupRepository _groupRepository;
+
+        public GroupController(IGroupRepository groupRepository)
+        {
+            _groupRepository = groupRepository;
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] CreateGroupDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto?.Name))
+                return BadRequest("Name is missing or empty");
+
+            long userId = GetUserId();
+
+            long groupId = await _groupRepository.CreateItem(userId, dto.Name);
+
+            Group storedGroup = await _groupRepository.GetById(groupId);
+            return Ok(Bind(storedGroup));
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Get([FromBody] CreateGroupDto dto)
+        {
+            long userId = GetUserId();
+
+            Group[] groups = await _groupRepository.GetMyGroups(userId);
+
+            return Ok(groups.Select(Bind).ToArray());
+        }
+
+        private long GetUserId()
+        {
+            return long.Parse((User.Identity as ClaimsIdentity)?.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
+
+        private GroupDto Bind(Group storedGroup)
+        {
+            return new GroupDto
+            {
+                Id = storedGroup.Id,
+                Name = storedGroup.Name
+            };
+        }
+
+
+    }
+
+    public class CreateGroupDto
+    {
+        public string Name { get; set; }
+    }
+
+    public class GroupDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Group
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long UserId { get; internal set; }
+    }
+}
---
+++ WebAPI/Dal/IGroupRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using WebAPI.Controllers;
+
+namespace WebAPI.Dal
+{
+    public interface IGroupRepository
+    {
+        Task<long> CreateItem(long userId, string name);
+        Task<Group> GetById(long groupId);
+        Task<Group[]> GetMyGroups(long userId);
+    }
+
+    public class GroupRepository : IGroupRepository
+    {
+        private static long Id;
+        private static object Locker = new object();
+        private static List<Group> groups = new List<Group>();
+
+        public Task<long> CreateItem(long userId, string name)
+        {
+            lock (Locker)
+            {
+                var group = new Group
+                {
+                    Id = ++Id,
+                    Name = name,
+                    UserId = userId
+                };
+
+                groups.Add(group);
+
+                return Task.FromResult(group.Id);
+            }
+        }
+
+        public Task<Group> GetById(long groupId)
+        {
+            lock (Locker)
+            {
+                return Task.FromResult(groups.SingleOrDefault(x => x.Id == groupId));
+            }
+        }
+
+        public Task<Group[]> GetMyGroups(long userId)
+        {
+            lock (Locker)
+            {
+                return Task.FromResult(groups.Where(x => x.UserId == userId).ToArray());
+            }
+        }
+    }
+}
